1ec7b27f937cb246c7ea304ff6f4b070e6d267c6
Wiggert <wloonstra@ebay.com>
2013-08-06 14:14:24 +0200

    536 Real locations _should_ appear in UI

--- src/com/bignerdranch/android/runtracker/RunFragment.java
+++ src/com/bignerdranch/android/runtracker/RunFragment.java
@@ -1,5 +1,9 @@
 package com.bignerdranch.android.runtracker;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,12 +11,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 public class RunFragment extends Fragment {
 
+    private BroadcastReceiver mLocationReceiver = new LocationReceiver() {
+
+        @Override
+        protected void onLocationReceived(Context context, Location loc) {
+            mLastLocation = loc;
+            if (isVisible())
+                updateUI();
+        }
+
+        @Override
+        protected void onProviderEnabledChanged(boolean enabled) {
+            int toastText = enabled ? R.string.gps_enabled : R.string.gps_disabled;
+            Toast.makeText(getActivity(), toastText, Toast.LENGTH_LONG).show();
+        }
+    };
+
     private RunManager mRunManager;
+
+    private Run mRun;
+    private Location mLastLocation;
 
     private Button mStartButton, mStopButton;
     private TextView mStartedTextView, mLatitudeTextView,
@@ -40,6 +64,7 @@ public class RunFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 mRunManager.startLocationUpdates();
+                mRun = new Run();
                 updateUI();
             }
         });
@@ -58,8 +83,34 @@ public class RunFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().registerReceiver(mLocationReceiver, new IntentFilter(RunManager.ACTION_LOCATION));
+    }
+
+    @Override
+    public void onStop() {
+        getActivity().unregisterReceiver(mLocationReceiver);
+        super.onStop();
+    }
+
     private void updateUI() {
         boolean started = mRunManager.isTrackingRun();
+
+        if (mRun != null) {
+            mStartedTextView.setText(mRun.getStartDate().toString());
+        }
+
+        int durationSeconds = 0;
+        if (mRun != null && mLastLocation != null) {
+            durationSeconds = mRun.getDurationSeconds(mLastLocation.getTime());
+            mLatitudeTextView.setText(Double.toString(mLastLocation.getLatitude()));
+            mLongitudeTextView.setText(Double.toString(mLastLocation.getLongitude()));
+            mAltitudeTextView.setText(Double.toString(mLastLocation.getAltitude()));
+        }
+        mDurationTextView.setText(Run.formatDuration(durationSeconds));
+
         mStartButton.setEnabled(!started);
         mStopButton.setEnabled(started);
     }
